866fd90d3e863ac53bca45c3410a4971f4459e3f
Esther0212 <eblacas.estherbeuthel@gmail.com>
2024-03-02 23:41:09 +0800

    3_2 Implement Create User in our User Table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 //use App\User;
 use App\Models\User; // <-- your model
+use Illuminate\Http\Response;
 use App\Traits\ApiResponser; // <-- use to standardized our code for apt response 
 use Illuminate\Http\Request; // <-- nandling http request in lumen
 use DB; // <-- if your not using lumen eloquent you can use DB component in lumen
